4385fc6b089b65631f2d9332ea74e91e491f9db5
Kalkavan Tugçe <tugce.kalkavan@telenetgroup.be>
2019-02-25 14:22:22 +0300

    fixed blank

--- Wallethubparser/spring-boot-wallethub/src/main/java/com/wallethub/WebparserApplication.java
+++ Wallethubparser/spring-boot-wallethub/src/main/java/com/wallethub/WebparserApplication.java
@@ -7,12 +7,13 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.List;
+
+//TK
 
 @SpringBootApplication
 public class WebparserApplication implements ApplicationRunner {
